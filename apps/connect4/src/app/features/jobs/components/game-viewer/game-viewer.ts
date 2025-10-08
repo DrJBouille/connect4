5ed8f90a769a18498d7e8dca37936624630ea091
DrJBouille <jeanbaptiste0605@gmail.com>
2025-10-08 22:45:35 +0200

    fix display board

--- apps/connect4/src/app/features/jobs/components/game-viewer/game-viewer.ts
+++ apps/connect4/src/app/features/jobs/components/game-viewer/game-viewer.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Move} from "../../models/Stats";
 import {MatIcon} from "@angular/material/icon";
 
@@ -10,17 +10,29 @@ import {MatIcon} from "@angular/material/icon";
   templateUrl: './game-viewer.html',
   styleUrl: './game-viewer.css',
 })
-export class GameViewer {
+export class GameViewer implements OnInit{
   @Input() moves!: Move[];
 
   protected moveId = 0;
+  protected board: number[][] = Array.from({ length: 6 }, () => Array(7).fill(0));
+
+  ngOnInit() {
+    this.changeMoveId(0)
+  }
 
   changeMoveId(newMoveId: number) {
     if (newMoveId < 0 || newMoveId > this.moves.length - 1) return;
     this.moveId = newMoveId;
+
+    this.board = Array.from({ length: 6 }, () => Array(7).fill(0));
+
+    for (let i = 0; i <= this.moveId; i++) {
+      const coordinate = this.moves[i].coordinate
+      this.board[coordinate.y][coordinate.x] = this.moves[i].isRedTurn ? 1 : 2;
+    }
   }
 
   get boardReversed() {
-    return [...this.moves[this.moveId].board].reverse();
+    return [...this.board].reverse();
   }
 }
